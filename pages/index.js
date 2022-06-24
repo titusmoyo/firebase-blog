5fde28ddbd1306683eb0f2ba24aa90448717a622
zimsecpapers <farmplus93@gmail.com>
2022-06-24 17:04:00 +0100

    Showing test data on HomePage

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,55 @@ import Image from 'next/image'
 // https://nextjs.org/docs/basic-features/pages
 
 import styles from '@styles/index.module.scss';
+import { getPosts } from '@lib/firebase';
 
-const HomePage = () => (
+const getFormattedDate = (milliseconds) => {
+  const formatOptions = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    timeZone: 'UTC',
+  };
+  const date = new Date(milliseconds);
+  return date.toLocaleDateString(undefined, formatOptions);
+};
+
+const HomePage = ({posts}) => (
+
   <div className={styles.HomePage}>
-    <h1>Hello from HomePage!</h1>
+    <h1>Blog Posts</h1>
+    {posts.map((post) => (
+      <article key={post.slug}>
+        <img src={post.coverImage} alt={post.coverImageAlt} />
+        <div>
+          <h2>{post.title}</h2>
+          <span>{getFormattedDate(post.dateCreated)}</span>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: `${post.content.substring(0, 200)}...`,
+            }}
+          ></p>
+        </div>
+      </article>
+    ))}
   </div>
+  
 );
+
+// This is for fetching data every time the page is visited. We do this
+// so that we don't have to redploy the site every time we add a blog post.
+// You can read more about this in the Next.js docs at:
+// https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+export async function getServerSideProps() {
+  const posts = await getPosts();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
 
 export default HomePage;
 
